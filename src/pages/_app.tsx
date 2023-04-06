45e88237c4b1f654bd3939d9e5fdac4cce10cd36
Ha Nguyen <hnn152151927@gmail.com>
2023-04-06 14:39:11 +0700

    chore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import "@/styles/globals.css"
 import { wagmiClient } from "@constants/wagmiConfigs"
+import useWallet from "@hooks/useWallet"
+import "@styles/globals.css"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
 import { useEffect } from "react"
 import { WagmiConfig } from "wagmi"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const {} = useWallet()
   useEffect(() => {
     wagmiClient.autoConnect()
   }, [])
